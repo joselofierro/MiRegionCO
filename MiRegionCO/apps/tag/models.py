754d4225e5faab53c5152babd895127a106da6fa
JULIO FIERRO <juliofierro@Mac-mini-de-JULIO.local>
2017-08-24 11:20:54 -0500

    correccion del nombre del tag

--- MiRegionCO/apps/tag/models.py
+++ MiRegionCO/apps/tag/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Tag(models.Model):
-    nombre = models.CharField(max_length=30, blank=False, null=False, unique=True)
+    nombre = models.CharField(max_length=100, blank=False, null=False, unique=True)
 
     def __str__(self):
         return self.nombre
