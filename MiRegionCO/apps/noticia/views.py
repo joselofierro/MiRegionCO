0ad139b94b3b9b81656326adb35d0bb1f63c4a9e
JULIO FIERRO <juliofierro@Mac-mini-de-JULIO.local>
2017-09-18 15:10:14 -0500

    no elimina noticias

--- MiRegionCO/apps/noticia/views.py
+++ MiRegionCO/apps/noticia/views.py
@@ -228,6 +228,5 @@ def eliminarnoticia(request, pk):
 
         noticia_obj = Noticia.objects.get(id=pk)
         noticia_obj.visible = False
-        noticia_obj.destacada = False
         noticia_obj.save()
         return redirect('noticia:listar')
