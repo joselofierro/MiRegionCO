58d2a3efe0ce91b53c7bd1b41f6dd0a299fef711
JULIO FIERRO <juliofierro@Mac-mini-de-JULIO.local>
2017-10-10 09:04:19 -0500

    Listado youtubers

--- MiRegionCO/api/views.py
+++ MiRegionCO/api/views.py
@@ -649,7 +649,7 @@ def FCM_CREATE(request):
 def ListadoConcursantes(request):
     if request.method == 'GET':
         concursantes = Youtuber.objects.all().values('codigo', 'foto', 'nombre').annotate(
-            votos=Count('votaciones__codigo')).order_by('-votaciones__codigo', 'nombre')
+            votos=Count('votaciones__codigo')).order_by('votaciones__codigo', 'nombre')
 
         return Response(concursantes, status=status.HTTP_200_OK)
 
@@ -659,8 +659,7 @@ class VotarYoutuber(CreateAPIView):
 
     def post(self, request, *args, **kwargs):
         if 'accessToken' in request.data:
-            if request.data['accessToken'] == 'ba517f21210bdf8e9e594f0f28257b020d9c0923' or request.data[
-                'accessToken'] == 'dbc6821f13c30a91738f280456f32513310c8aa4':
+            if request.data['accessToken'] == 'ba517f21210bdf8e9e594f0f28257b020d9c0923' or request.data['accessToken'] == 'dbc6821f13c30a91738f280456f32513310c8aa4':
                 request.data.pop('accessToken')
                 obj_votacion = VotacionSerializer(data=request.data)
                 if obj_votacion.is_valid():
