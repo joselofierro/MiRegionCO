f5e9ec43538589858f566c60543b5e75566b5b14
JULIO FIERRO <juliofierro@Mac-mini-de-JULIO.local>
2017-11-03 17:06:22 -0500

    no mas noticias

--- MiRegionCO/apps/activacion_youtuber/views.py
+++ MiRegionCO/apps/activacion_youtuber/views.py
@@ -8,7 +8,7 @@ from apps.activacion_youtuber.forms import YoutuberForm
 from apps.activacion_youtuber.models import Youtuber
 
 
-class YoutuberCreate(PermissionRequiredMixin, SuccessMessageMixin,CreateView):
+class YoutuberCreate(PermissionRequiredMixin, SuccessMessageMixin, CreateView):
     model = Youtuber
     form_class = YoutuberForm
     template_name = 'youtuber/youtuber_form.html'
@@ -20,7 +20,7 @@ class YoutuberCreate(PermissionRequiredMixin, SuccessMessageMixin,CreateView):
     redirect_field_name = 'redirect_to'
 
 
-class YoutuberList(PermissionRequiredMixin,ListView):
+class YoutuberList(PermissionRequiredMixin, ListView):
     model = Youtuber
     template_name = 'youtuber/youtuber_list.html'
     permission_required = ('activacion_youtuber.add_youtuber', 'activacion_youtuber.change_youtuber')
@@ -29,7 +29,7 @@ class YoutuberList(PermissionRequiredMixin,ListView):
     redirect_field_name = 'redirect_to'
 
 
-class YoutuberUpdate(PermissionRequiredMixin, SuccessMessageMixin,UpdateView):
+class YoutuberUpdate(PermissionRequiredMixin, SuccessMessageMixin, UpdateView):
     model = Youtuber
     form_class = YoutuberForm
     template_name = 'youtuber/youtuber_form.html'
@@ -41,13 +41,13 @@ class YoutuberUpdate(PermissionRequiredMixin, SuccessMessageMixin,UpdateView):
     redirect_field_name = 'redirect_to'
 
 
-class YoutuberDelete(PermissionRequiredMixin, SuccessMessageMixin,DeleteView):
+class YoutuberDelete(PermissionRequiredMixin, SuccessMessageMixin, DeleteView):
     model = Youtuber
     template_name = 'youtuber/youtuber_delete.html'
     success_url = reverse_lazy('youtuber:listar')
     success_message = 'Se ha eliminado el youtuber correctamente'
-    permission_required = ('activacion_youtuber.add_youtuber', 'activacion_youtuber.change_youtuber', 'activacion_youtuber.delete_youtuber')
+    permission_required = (
+    'activacion_youtuber.add_youtuber', 'activacion_youtuber.change_youtuber', 'activacion_youtuber.delete_youtuber')
     raise_exception = False
     login_url = reverse_lazy('grupo:login')
     redirect_field_name = 'redirect_to'
-
